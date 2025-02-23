93004ef4470b923486937e2b21715be4e62a3732
Sheel Patel <sheelpatel5252@gmail.com>
2025-02-23 16:39:33 -0500

    learning

---
+++ bot.py
@@ -0,0 +1,11 @@
+import os
+from slack_sdk import WebClient
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#test', text='Hello World!')
